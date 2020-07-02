d95660e2b7df993c9f7d65e1c466f39ded225e69
xstefank <xstefank122@gmail.com>
2020-07-02 11:49:27 +0200

    Add XP version bump to eap-version-bump.sh

--- eap-version-bump.sh
+++ eap-version-bump.sh
@@ -42,9 +42,14 @@ readonly PRODUCT_POM=${PRODUCT_POM:-'./pom.xml'}
 readonly PRODUCT_VERSION_TXT=${PRODUCT_VERSION_TXT:-'feature-pack/src/main/resources/content/version.txt'}
 readonly PRODOCT_VERSION_ALT_TXT=${PRODOCT_VERSION_ALT_TXT:-'galleon-pack/src/main/resources/packages/version.txt/content/version.txt'}
 readonly PRODUCT_VERSION="${NEXT_VERSION}.GA"
+readonly XP_MINOR_VERSION=$(echo $NEXT_VERSION | sed -e "s/.*\.\(.*\)$/\1/")
+readonly XP_PRODUCT_VERSION="1.0.$( expr "${XP_MINOR_VERSION}" - 1 ).GA"
 
 echo -n "Update ${PRODUCT_POM} and ${PRODUCT_VERSION_TXT} to ${PRODUCT_VERSION}..."
 sed -i "${PRODUCT_POM}" -e "s;\(<full.dist.product.release.version>\)[^<]*\(.*$\);\1${PRODUCT_VERSION}\2;"
 sed -i "${PRODUCT_VERSION_TXT}" -e "s;\(^.* Version \).*;\1${PRODUCT_VERSION};"
 sed -i "${PRODOCT_VERSION_ALT_TXT}" -e "s;\(Red Hat JBoss Enterprise Application Platform - Version \).*;\1${PRODUCT_VERSION};"
+echo 'Done.'
+echo -n "Update ${PRODUCT_POM} to XP version ${XP_PRODUCT_VERSION}..."
+sed -i "${PRODUCT_POM}" -e "s;\(<expansion.pack.release.version>\)[^<]*\(.*$\);\1${XP_PRODUCT_VERSION}\2;"
 echo 'Done.'
