d058fbc9d527524cc132ce81fa4005dc193f0bae
Lin Gao <aoingl@gmail.com>
2020-12-15 13:40:26 +0800

    [SET-321] Fix eap-version-bump.sh to match the changes in EAP code base
    File: 'galleon-pack/src/main/resources/packages/version.txt/content/version.txt' does not exist anymore

--- eap-version-bump.sh
+++ eap-version-bump.sh
@@ -10,9 +10,10 @@ NEXT_VERSION=${2}
 PREVIOUS_XP_VERSION=${3}
 NEXT_XP_VERSION=${4}
 
+readonly PRODUCT_VERSION_TXT=${PRODUCT_VERSION_TXT:-'feature-pack/src/main/resources/content/version.txt'}
 readonly PRODUCT_VERSION_ALT_TXT=${PRODUCT_VERSION_ALT_TXT:-'galleon-pack/src/main/resources/packages/version.txt/content/version.txt'}
 readonly PRODUCT_VERSION_REGEXP='Red Hat JBoss Enterprise Application Platform - Version (.*)'
-CURRENT_PRODUCT_VERSION=$(cat "${PRODUCT_VERSION_ALT_TXT}")
+CURRENT_PRODUCT_VERSION=$(cat "${PRODUCT_VERSION_TXT}")
 
 if [[ "${CURRENT_PRODUCT_VERSION}" =~ ${PRODUCT_VERSION_REGEXP} ]]; then
   version="${BASH_REMATCH[1]}"
@@ -58,7 +59,6 @@ find . -name pom.xml -exec sed -i "s/${FROM}/${TO}/g" '{}' \;
 echo 'Done.'
 
 readonly PRODUCT_POM=${PRODUCT_POM:-'./pom.xml'}
-readonly PRODUCT_VERSION_TXT=${PRODUCT_VERSION_TXT:-'feature-pack/src/main/resources/content/version.txt'}
 readonly PRODUCT_VERSION="${NEXT_VERSION}.GA"
 readonly XP_MINOR_VERSION=$(echo "${NEXT_VERSION}" | sed -e "s/.*\.\(.*\)$/\1/")
 readonly XP_PRODUCT_VERSION="1.0.$( expr "${XP_MINOR_VERSION}" - 1 ).GA"
@@ -66,7 +66,14 @@ readonly XP_PRODUCT_VERSION="1.0.$( expr "${XP_MINOR_VERSION}" - 1 ).GA"
 echo -n "Update ${PRODUCT_POM} and ${PRODUCT_VERSION_TXT} to ${PRODUCT_VERSION}..."
 sed -i "${PRODUCT_POM}" -e "s;\(<full.dist.product.release.version>\)[^<]*\(.*$\);\1${PRODUCT_VERSION}\2;"
 sed -i "${PRODUCT_VERSION_TXT}" -e "s;\(^.* Version \).*;\1${PRODUCT_VERSION};"
-sed -i "${PRODUCT_VERSION_ALT_TXT}" -e "s;\(Red Hat JBoss Enterprise Application Platform - Version \).*;\1${PRODUCT_VERSION};"
+echo 'Done.'
+if [ -e "${PRODUCT_VERSION_ALT_TXT}" ]; then
+  echo -n "Update ${PRODUCT_VERSION_ALT_TXT} to ${PRODUCT_VERSION}..."
+  sed -i "${PRODUCT_VERSION_ALT_TXT}" -e "s;\(Red Hat JBoss Enterprise Application Platform - Version \).*;\1${PRODUCT_VERSION};"
+  echo 'Done.'
+else
+  echo "Please note that there is no such file '${PRODUCT_VERSION_ALT_TXT}'"
+fi
 
 echo -n "Update ${PRODUCT_POM} to XP version ${XP_PRODUCT_VERSION}..."
 sed -i "${PRODUCT_POM}" -e "s;\(<expansion.pack.release.version>\)[^<]*\(.*$\);\1${NEXT_XP_VERSION}\2;"
